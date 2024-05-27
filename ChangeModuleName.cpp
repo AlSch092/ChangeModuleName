e64236d508f975157fde592e2cf1d52dd4cd8195
Alex S <94417808+AlSch092@users.noreply.github.com>
2024-05-26 20:05:26 -0400

    added `HideModuleFromPEB`

--- ChangeModuleName.cpp
+++ ChangeModuleName.cpp
@@ -67,23 +67,69 @@ typedef struct _MYPEB {
 
 void ChangeModuleName(wchar_t* szModule, wchar_t* newName)
 {
-	PPEB PEB = (PPEB)__readgsqword(0x60);  //we are compiling as x64!
+	PPEB PEB = (PPEB)__readgsqword(0x60);
 	_LIST_ENTRY* f = PEB->Ldr->InMemoryOrderModuleList.Flink;
 	bool Found = FALSE;
 
-	while (!Found)
+	int Counter = 0;
+
+	while (!Found && Counter < 100)
 	{
 		PLDR_DATA_TABLE_ENTRY dataEntry = CONTAINING_RECORD(f, LDR_DATA_TABLE_ENTRY, InMemoryOrderLinks);
 
 		if (wcsstr(dataEntry->FullDllName.Buffer, szModule) != NULL)
 		{
 			wcscpy(dataEntry->FullDllName.Buffer, newName);
+			dataEntry->FullDllName.Length = wcslen(newName) + 1;
+			dataEntry->FullDllName.MaximumLength = wcslen(newName) + 1;
 			Found = TRUE;
 			return;
 		}
 
 		f = dataEntry->InMemoryOrderLinks.Flink;
+		Counter += 1;
 	}
+}
+
+/*
+	HideModuleFromPEB - erases module name from PEB LDR
+*/
+BOOL HideModuleFromPEB(const wchar_t* moduleName)
+{
+	PPEB pPEB = (PPEB)__readgsqword(0x60);
+
+	if (!pPEB)
+	{
+		return FALSE;
+	}
+
+	PPEB_LDR_DATA pLdr = pPEB->Ldr;
+
+	if (!pLdr)
+	{
+		return FALSE;
+	}
+
+	PLIST_ENTRY pListHead = &pLdr->InMemoryOrderModuleList;
+	PLIST_ENTRY pListEntry = pListHead->Flink;
+	int Counter = 0;
+
+	while (pListEntry != pListHead && Counter < 100)
+	{
+		PLDR_DATA_TABLE_ENTRY pEntry = CONTAINING_RECORD(pListEntry, LDR_DATA_TABLE_ENTRY, InMemoryOrderLinks);
+
+		if (wcsstr(pEntry->FullDllName.Buffer, moduleName) != 0)
+		{
+			memset(pEntry->FullDllName.Buffer, 0, pEntry->FullDllName.Length); //null'ing all info about the dll name results in some tools such as cheat engine being unable to parse all module symbols (no DLLs or symbols will show up in moduleList)
+			pEntry->FullDllName.Length = 0;  //calls to GetModuleHandle() will also fail
+			pEntry->FullDllName.MaximumLength = 0;
+			return TRUE;
+		}
+
+		pListEntry = pListEntry->Flink;
+		Counter += 1;
+	}
+	return FALSE;
 }
 
 int main(int argc, char** argv)
@@ -91,6 +137,8 @@ int main(int argc, char** argv)
 	ChangeModuleName((wchar_t*)L"KERNEL32.DLL", (wchar_t*)L"Renamed.DLL"); //change K32 to something else
 	ChangeModuleName((wchar_t*)L"USER32.DLL", (wchar_t*)L"Renamed.DLL"); //example of duplicate module names
 	ChangeModuleName((wchar_t*)L"changeModuleName.exe", (wchar_t*)L""); //example of technique working on host EXE, and with a NULL string.
+	
+	HideModuleFromPEB(L"Renamed.DLL");
 	system("pause");
 	return 0;
 }
